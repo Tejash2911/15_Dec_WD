f457edf680ebe9ff04fc29de56e8ce3ede0e8a9f
Tejash Patel <108259942+Tejash2911@users.noreply.github.com>
2023-06-30 13:19:13 +0530

    Add files via upload

---
+++ Assessment/Assessment4/script.js
@@ -0,0 +1,38 @@
+let todoList = new Map();
+
+function addTodo() {
+    let todoName = document.querySelector('#todoName').value;
+    if (todoName.length == 0) {
+        alert("Please Enter a task😄");
+    } else {
+        todoList.set(todoName, todoName);
+        printList();
+        document.querySelector('#todoName').value = "";
+    }
+}
+
+function printList() {
+    let str = "";
+    todoList.forEach((value, key, map) => {
+        str += `<div class="task">
+                    <span id="taskname">${value}</span>
+                    <button id=${key} onclick="deleteTodo(this.id)">Delete</button>
+                </div>`;
+    })
+    document.querySelector("#todoList").innerHTML = str;
+}
+
+function deleteTodo(id) {
+    todoList.delete(id);
+    printList();
+}
+
+function clearTodo() {
+    if (todoList.size == 0) {
+        alert("No Items To Clear😁")
+    } else {
+        todoList.clear();
+    }
+    printList();
+}
+
